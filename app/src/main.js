3c502031b262ee8f92ceaefbd1ba50d51a661365
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2015-05-14 22:41:48 -0400

    main: Fetch product data on load
    Fetch products and categories during the main bootstrap. Once the
    data has been fetched, update the products cursor with a merge of
    the two datasets.

--- app/src/main.js
+++ app/src/main.js
@@ -7,6 +7,9 @@ Backbone.$ = $;
 
 import app from './app';
 import stateTree from './state_tree';
+import Products from './collections/products';
+import Categories from './collections/categories';
+import { updateProducts } from './actions';
 
 $(function() {
     app.init({
@@ -14,4 +17,24 @@ $(function() {
         state: stateTree
     });
     app.layout.render();
+
+    const products = new Products();
+    const categories = new Categories();
+
+    $.when(products.fetch(), categories.fetch()).done(() => {
+        const categoryData = categories.toJSON();
+
+        updateProducts(
+            products.toJSON().map(product => {
+                return {
+                    category: categoryData
+                        .filter(category => category.id === product.categoryId)
+                        .name,
+                    price: product.price,
+                    stocked: product.stocked,
+                    name: product.name
+                };
+            })
+        );
+    });
 });
